5c8f5f3cf716fff714717ce75353984c994da074
bugbytes-io <bugbytes.io@gmail.com>
2022-02-13 17:39:40 +0000

    Code for accordion example

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.courses, name='courses'),
-    path('modules/', views.modules, name='modules')
+    path('modules/', views.modules, name='modules'),
+    path('info/', views.index, name='index'),
+    path('courselist/', views.course_list, name='course-list')
+
 ]
--- core/views.py
+++ core/views.py
@@ -13,3 +13,13 @@ def modules(request):
     form = UniversityForm(request.GET)
     # print(form['modules'])  # form['modules'] gives us HTML for <select>
     return HttpResponse(form['modules'])
+
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)    
+
+def course_list(request):
+    courses = Course.objects.prefetch_related('modules').all()
+    context = {'courses': courses}
+    return render(request, 'partials/course-list.html', context)
